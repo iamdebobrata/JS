3c6f503998e9c2861f170fde8571b8980581b0f4
DEBOBRATA PAUL <117559790+iamdebobrata@users.noreply.github.com>
2024-04-18 21:42:11 +0000

    datatype Summury

---
+++ 01_Basics/datatypes-summury.js
@@ -0,0 +1,45 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 427267876736n
+
+
+
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+
+
+let myObj = {
+    name: "Debobrata",
+    age: 23,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+console.log(typeof bigNumber);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof anotherId);
