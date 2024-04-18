df75407ffb80db3977891808cc3f052d2f9d1ace
DEBOBRATA PAUL <117559790+iamdebobrata@users.noreply.github.com>
2024-04-18 22:35:06 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,40 @@
+const name = "Debobrata"
+const repoCount = 5
+
+// console.log(name + repoCount + " value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+const gameName = new String('Debobrata')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('u'));
+
+
+// const newString = gameName.substring(0,2)
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-7,4)
+// console.log(anotherString);
+
+const newStringOne = "      Debobrata     "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+const url = "https://debobrata.com/debobrata%20paul"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('debobrata'))
+
+//split method use mdn
+
