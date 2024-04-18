21a786be3b45ee750f69f23808c4835afa379cf2
DEBOBRATA PAUL <117559790+iamdebobrata@users.noreply.github.com>
2024-04-18 20:31:22 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict"; // traet all js code as newer version
+
+// alert(3 + 3) we are using node.js , not browser
+
+
+// code readibility shiuld be high
+// console.log(3 + 3); console.log("Debobrata");
+
+let name = "Debobrata"
+let age = 23
+let isLogin = false
+let state;
+
+
+// number => 2 to power 53
+// bigint => For large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+
+// object 
+
+
+console.log(typeof "Debobrata");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof state);
+console.log(typeof undefined); // undefined
+
+
