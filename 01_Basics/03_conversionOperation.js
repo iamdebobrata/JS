6432acd4ce24a201131fd807848bee658fd60c0f
DEBOBRATA PAUL <117559790+iamdebobrata@users.noreply.github.com>
2024-04-18 20:48:34 +0000

    converstion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33abc" => NaN
+// true = > 1; false => 0
+
+
+// let isLoggedIn = "Debobrata"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Debobrata" => true
+
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
